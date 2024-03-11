c1c2bd74edd08d563cb4ad35da67d5eef7153e2e
Eugene <157716580+euyshtho@users.noreply.github.com>
2024-03-11 10:29:52 -0400

    Added ALU Testbench

---
+++ ALU/alu_tb.v
@@ -0,0 +1,30 @@
+/*
+CS 274
+Ursinus College
+
+@author Eugene Thompson
+@purpose Implementation of ALU
+*/
+`include "alu.v"
+`default_nettype none
+
+
+////////////////////////////////////////////////////////////
+//               LOGIC CLASS OF OPERATIONS               //
+///////////////////////////////////////////////////////////
+
+//Circuit 1: 20-bit NOT
+
+module not_ops_tb;
+    reg a;
+    wire c;
+    wire zero;
+
+    not_ops x0 (
+        .a(a),
+        .c(c),
+        .zero(zero)
+    );
+
+
+endmodule
