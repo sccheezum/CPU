c912600340d61520dfdcb0d06ee1530068847cca
Eugene <157716580+euyshtho@users.noreply.github.com>
2024-03-14 15:21:37 -0400

    Rest of the Modules done with Local Testing Passed!

--- ALU/alu_tb.v
+++ ALU/alu_tb.v
@@ -45,3 +45,102 @@ module not_ops_tb;
         end
     end
 endmodule
+
+//Circuit 2: 20-bit AND
+module and_ops_tb;
+    reg clk;
+    reg [19:0] a;
+    reg [19:0] b;
+    wire [19:0] c;
+    wire zero;
+
+    integer i;
+    integer MAX_ITERS = 10;
+    integer SEED = 10559;
+
+    and_ops x0 (
+        .a(a),
+        .b(b),
+        .c(c),
+        .zero(zero)
+    );
+
+    initial begin
+        a <= 0;
+        b <= 0;
+
+        $monitor ("a: %b - b: %b - c: %b - zero: %b", a, b, c, zero);
+
+        for (i = 0; i < MAX_ITERS; i++)begin
+            #10 clk <= ~clk;
+                a <= $urandom(SEED);
+                b <= $urandom(SEED);
+        end
+    end
+endmodule
+
+//Circuit 3: 20-bit OR
+module or_ops_tb;
+    reg clk;
+    reg [19:0] a;
+    reg [19:0] b;
+    wire [19:0] c;
+    wire zero;
+
+    integer i;
+    integer MAX_ITERS = 10;
+    integer SEED = 10559;
+
+    or_ops x0 (
+        .a(a),
+        .b(b),
+        .c(c),
+        .zero(zero)
+    );
+
+    initial begin
+        a <= 0;
+        b <= 0;
+
+        $monitor ("a: %b - b: %b - c: %b - zero: %b", a, b, c, zero);
+
+        for (i = 0; i < MAX_ITERS; i++)begin
+            #10 clk <= ~clk;
+                a <= $urandom(SEED);
+                b <= $urandom(SEED);
+        end
+    end
+endmodule
+
+//Circuit 4: 20-bit XOR
+module xor_ops_tb;
+    reg clk;
+    reg [19:0] a;
+    reg [19:0] b;
+    wire [19:0] c;
+    wire zero;
+
+    integer i;
+    integer MAX_ITERS = 10;
+    integer SEED = 10559;
+
+    xor_ops x0 (
+        .a(a),
+        .b(b),
+        .c(c),
+        .zero(zero)
+    );
+
+    initial begin
+        a <= 0;
+        b <= 0;
+
+        $monitor ("a: %b - b: %b - c: %b - zero: %b", a, b, c, zero);
+
+        for (i = 0; i < MAX_ITERS; i++)begin
+            #10 clk <= ~clk;
+                a <= $urandom(SEED);
+                b <= $urandom(SEED);
+        end
+    end
+endmodule
